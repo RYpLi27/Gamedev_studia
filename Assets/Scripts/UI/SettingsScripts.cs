1a585913026602203c29b1445fb87dbfec83c109
ZinneX <robertmazurkiewicz1112@gmail.com>
2025-01-17 23:37:58 +0100

    Ustawienia
    Fulscreen i resolution

--- Assets/Scripts/UI/SettingsScripts.cs
+++ Assets/Scripts/UI/SettingsScripts.cs
@@ -1,17 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class SettingsScripts : MonoBehaviour {
-    [SerializeField] private Toggle fullscreenToggle; // NIE WIDAC W EDYTORZE
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private TMP_Dropdown resolutionChange;
 
-    private void Start() {
-        fullscreenToggle.value = Screen.fullScreen;
-        GetComponent<Canvas>().enabled = false;
+    public void SetValues() {
+        fullscreenToggle.isOn = Screen.fullScreen;
+
+        print(Screen.height);
+        
+        resolutionChange.value = Screen.height switch {
+            1440 => 0,
+            1080 => 1,
+            720 => 2,
+            480 => 3,
+            _ => resolutionChange.value
+        };
     }
 
     public void FullscreenToggle() {
-        Screen.fullScreen = fullscreenToggle.value;
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void ResolutionChange() {
+        switch (resolutionChange.value) {
+            case 0:
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
+                break;
+            
+            case 1:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            
+            case 2:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+            
+            case 3:
+                Screen.SetResolution(640, 480, Screen.fullScreen);
+                break;
+        }
     }
 }
